08a74b2933351fd0049c789db4dc5c6ad18cf799
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-10 14:51:53 +0600

    Add new files and update project configuration

---
+++ BrainStationAPI/src/main/java/com/brainstation/BrainStationAPI/BrainStationApiApplication.java
@@ -0,0 +1,13 @@
+package com.brainstation.BrainStationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainStationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainStationApiApplication.class, args);
+	}
+
+}
---
+++ BrainStationAPI/src/test/java/com/brainstation/BrainStationAPI/BrainStationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainstation.BrainStationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainStationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
