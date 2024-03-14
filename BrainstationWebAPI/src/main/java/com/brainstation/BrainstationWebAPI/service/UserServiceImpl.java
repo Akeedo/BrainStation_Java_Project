8a5be39bc3e9a5531d0900ee2cc5315f2736a5af
Akeedo <akeedanjumoffice@gmail.com>
2024-03-14 20:43:38 +0600

    Update updateUerById i n UserServiceImpl

--- BrainstationWebAPI/src/main/java/com/brainstation/BrainstationWebAPI/service/UserServiceImpl.java
+++ BrainstationWebAPI/src/main/java/com/brainstation/BrainstationWebAPI/service/UserServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.*;
 
@@ -24,12 +25,12 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
 
-    public boolean updateLocation(String user){
-        kafkaTemplate.send(AppConstant.USER_SAVE, user);
+    public boolean updateUserMessageForKafka(String user){
+        kafkaTemplate.send(AppConstant.USER_CRUD, user);
         return true;
     }
 
-    @KafkaListener(topics = "user-save", groupId = "user-group")
+    @KafkaListener(topics = "user-crud", groupId = "user-group")
     public void userConsumer(String user){
         System.out.println(user);
     }
@@ -40,15 +41,8 @@ public class UserServiceImpl implements UserService {
             User _user = userRepository
                     .save(new User(user.getUsername(), user.getEmail(),user.getPassword()));
             Map<String, String> result = Map.of("message", "The user is created");
-            Thread.sleep(1000);
-            UserResponse response = new UserResponse(_user, result);
-
-            int range = 100;
-            while (range > 0){
-                updateLocation(Math.random() + " , " + Math.random());
-                range--;
-            }
-
+           UserResponse response = new UserResponse(_user, result);
+            updateUserMessageForKafka(user.getUsername()+ " is created");
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
         } catch (Exception e) {
             throw (e);
@@ -62,6 +56,7 @@ public class UserServiceImpl implements UserService {
         try {
             List<User> users = new ArrayList<User>();
             users = userRepository.findAll();
+            updateUserMessageForKafka("Getting all the users");
             return new ResponseEntity<>(users, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -70,9 +65,38 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public ResponseEntity<User> getUerById(@PathVariable("id") long id) {
-        Optional<User> userExist = userRepository.findById(id);
+        try{
+            Optional<User> userExist = userRepository.findById(id);
+            updateUserMessageForKafka("Get the user of this id= " + String.valueOf(id));
+            return userExist.map(user -> new ResponseEntity<>(user, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
-        return userExist.map(user -> new ResponseEntity<>(user, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public ResponseEntity<User> updateUerById(@PathVariable("id") long id, @RequestBody User userUpdate) {
+        try{
+            if (!userRepository.existsById(id)) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            Optional<User> userData = userRepository.findById(id);
+            User user = userRepository.findById(id)
+                    .map(existingUser -> {
+                        existingUser.setUsername(userUpdate.getUsername());
+                        existingUser.setEmail(userUpdate.getEmail());
+                        return userRepository.save(existingUser);
+                    })
+                    .orElseThrow(() -> new RuntimeException("Unexpected error during user update"));
+            updateUserMessageForKafka("Get the user of this id= " + String.valueOf(id));
+            return ResponseEntity.ok(user);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
 
@@ -80,6 +104,7 @@ public class UserServiceImpl implements UserService {
     public ResponseEntity<HttpStatus> deleteById(@PathVariable("id") long id) {
         try {
             userRepository.deleteById(id);
+            updateUserMessageForKafka("Get the user of this id= " + String.valueOf(id));
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
