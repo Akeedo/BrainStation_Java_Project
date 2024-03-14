2b86e221e1ba6a419563b64e316d774ac3173b1e
Akeedo <akeedanjumoffice@gmail.com>
2024-03-14 20:41:44 +0600

    Change the AppConstant

--- BrainstationWebAPI/src/main/java/com/brainstation/BrainstationWebAPI/constant/AppConstant.java
+++ BrainstationWebAPI/src/main/java/com/brainstation/BrainstationWebAPI/constant/AppConstant.java
@@ -1,5 +1,5 @@
 package com.brainstation.BrainstationWebAPI.constant;
 
 public class AppConstant {
-    public static final String USER_SAVE = "user-save";
+    public static final String USER_CRUD = "user-crud";
 }
