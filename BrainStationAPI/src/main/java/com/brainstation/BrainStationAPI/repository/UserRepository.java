6080737eb4ed1af7f3617e2924ffcf0463e67142
Akeedo <akeedanjumoffice@gmail.com>
2024-03-16 23:30:24 +0600

    Remove unnessary project

--- BrainStationAPI/src/main/java/com/brainstation/BrainStationAPI/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.brainstation.BrainStationAPI.repository;
-
-import com.brainstation.BrainStationAPI.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
